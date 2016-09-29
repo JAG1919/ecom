2873a8c2da3442c3e0748e6853b65abd10c20ef3
Justin G <jayglaz@gmail.com>
2016-09-28 22:05:12 -0400

    Added registration and requirements

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     # url(r'^$', 'bookstore.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^store/', 'store.views.store', name='store'),
+    url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
